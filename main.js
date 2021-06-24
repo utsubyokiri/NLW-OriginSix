77dcdc56479d1a38dde79016d474b05886b6ada5
Everton Araújo Santos <etuetuetuetu25@gmail.com>
2021-06-24 09:45:13 -0300

    NLW OriginSix day3

--- main.js
+++ main.js
@@ -19,12 +19,42 @@ for (const link of links) {
 
 //CHANGE PAGE HEADER WHEN SCROLLED
 const header = document.querySelector('#header')
-const navHeight = header.offsetHeight 
+const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function() {
-  if(window.scrollY >= navHeight) {
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
     header.classList.remove('scroll')
   }
-}) 
+})
+
+/*TESTIMONIALS CAROUSEL SLIDER SWIPER */
+
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+//SCROLL REVEAL : SHOW ELEMENTS WHEN SCROLLING
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `
+#home .text , #home .image,
+#about .image, #about .text,
+#services header, #services .card,
+#testimonials header, #testimonials .testimonials
+#contact .text, #contact links
+`,
+  { interval: 100 }
+)
